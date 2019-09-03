193458e659d42020854c9fb6ff3e2920fc8e1099
cesarvillamilruiz <cesar.hades77@gmail.com>
2019-09-02 19:23:01 -0500

    ProductoTerminado

--- Quileia/Quileia/Data/Entities/Ciudad.cs
+++ Quileia/Quileia/Data/Entities/Ciudad.cs
@@ -12,18 +12,20 @@ namespace Quileia.Data.Entities
 
         [Display(Name ="Ciudad")]
         [StringLength(50,ErrorMessage ="Maximo {1} caracteres")]
-
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string NombreCiudad { get; set; }
 
         [Display(Name = "Numero de habitantes")]
-        [Range(1,8000000,ErrorMessage ="minimo {1} habitante y maximo {2} habitantes")]
+        [Range(0,8000000,ErrorMessage ="minimo {1} habitante y maximo {2} habitantes")]
         public int CantidaddeHabitantes { get; set; }
 
         [Display(Name = "Lugar Turistico")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         [StringLength(50, ErrorMessage = "Maximo {1} caracteres")]
         public string SitioTuristico { get; set; }
 
         [Display(Name = "Mejor hotel")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         [StringLength(50, ErrorMessage = "Maximo {1} caracteres")]
         public string  HotelMasReservado { get; set; }
         public List<TuristaCiudad> TuristaCiudades { get; set; }
--- Quileia/Quileia/Data/Entities/Turista.cs
+++ Quileia/Quileia/Data/Entities/Turista.cs
@@ -9,16 +9,32 @@ namespace Quileia.Data.Entities
     public class Turista:IEntity
     {
         public int Id { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Display(Name ="Nombre Completo")]
+      
         public string NombreCompleto { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd }", ApplyFormatInEditMode = true)]
+
+        [Display(Name ="Fecha de nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime? FechaDeNacimiento { get; set; }
+
+        
+        [Range(0,10000000000000,ErrorMessage ="Debe ser un valor entre {1} y {2}")]
         public string Identificacion { get; set; }
-        public int FrecunciaDeViaje { get; set; }   
+
+        [Display(Name ="Frecuencia de viaje")]
+        [Range(0,15,ErrorMessage ="debe ser un vlor entre {1} y {2}")]
+        public int FrecunciaDeViaje { get; set; }
+        
+        [Range(0,200000000,ErrorMessage ="Debe ser un valor entre {1} y {2}")]
+        [Display(Name ="Presupuesto de viaje")]
         public double PresupuestoDeViaje { get; set; }
         public List<TuristaCiudad> TuristaCiudades { get; set; }
+
+
+        [Display(Name =" Usa tarjeta?")]
         public bool UsaTarjeta { get; set; }
 
-       
     }
 }
